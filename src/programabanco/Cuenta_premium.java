3e30daf9d417779c39aec30907944f7121a4c9b0
camilavaleria4 <camilavaleria4@hotmail.com>
2023-08-26 12:14:07 -0500

    Clases de cuentas y avance de ahorro

---
+++ src/programabanco/Cuenta_premium.java
@@ -0,0 +1,14 @@
+package programabanco;
+
+public class Cuenta_premium {
+    String numero_cuenta;
+    String nombre;
+    
+    public Cuenta_premium(){
+        nombre="Cuenta premium";
+    }
+    
+    public Cuenta_premium(String numero_cuenta){
+        this.numero_cuenta=numero_cuenta;
+    }    
+}
