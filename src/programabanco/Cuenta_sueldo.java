824554300797e1b650cc3053eb9806b2dd23fc6e
camilavaleria4 <camilavaleria4@hotmail.com>
2023-08-26 12:15:51 -0500

    Clases de cuentas y avance de ahorro

---
+++ src/programabanco/Cuenta_sueldo.java
@@ -0,0 +1,15 @@
+package programabanco;
+
+public class Cuenta_sueldo {
+    String numero_cuenta;
+    String nombre;
+    
+    
+    public Cuenta_sueldo(){
+        nombre="Cuenta sueldo";
+    }
+    
+    public Cuenta_sueldo(String numero_cuenta){
+        this.numero_cuenta=numero_cuenta;
+    }    
+}
