c567459baeff3171c8695609cc304230badb40f2
camilavaleria4 <camilavaleria4@hotmail.com>
2023-08-26 12:17:47 -0500

    Clases de cuentas y avance de ahorro

--- src/programabanco/ProgramaBanco.java
+++ src/programabanco/ProgramaBanco.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package programabanco;
 
-/**
- *
- * @author Usuario
- */
 public class ProgramaBanco {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        //hola mundo
         
     }
     
