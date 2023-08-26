5373b4b1b67d39506c70c37e464075518ff2566d
camilavaleria4 <camilavaleria4@hotmail.com>
2023-08-26 12:16:18 -0500

    Clases de cuentas y avance de ahorro

---
+++ src/programabanco/Cuenta_de_ahorro.java
@@ -0,0 +1,14 @@
+package programabanco;
+
+public class Cuenta_de_ahorro {
+    String numero_cuenta;
+    String nombre;
+    
+    public Cuenta_de_ahorro(){
+        nombre="Cuenta de ahorro";
+    }
+    
+    public Cuenta_de_ahorro(String numero_cuenta){
+        this.numero_cuenta=numero_cuenta;
+    }    
+}
