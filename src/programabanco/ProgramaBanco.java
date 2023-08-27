aa4647b6b189cabf110f6ab40a8c76fda08e67ac
camilavaleria4 <camilavaleria4@hotmail.com>
2023-08-27 16:53:29 -0500

    Avance cuenta premium

--- src/programabanco/ProgramaBanco.java
+++ src/programabanco/ProgramaBanco.java
@@ -3,6 +3,10 @@ package programabanco;
 public class ProgramaBanco {
 
     public static void main(String[] args) {
+        Cuenta_premium cuenta_uno= new Cuenta_premium("111111", "Cuenta premium", 10000);
+        cuenta_uno.imprimir_datos();
+        cuenta_uno.setdeposito(5000);
+        cuenta_uno.imprimir_interes();
         
     }
     
