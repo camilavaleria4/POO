f6ad0634fcadc5661c93c5bd9a8cc46182e08bcb
camilavaleria4 <camilavaleria4@hotmail.com>
2023-08-27 21:23:09 -0500

    Avance cuenta premium

--- src/programabanco/Ahorro.java
+++ src/programabanco/Ahorro.java
@@ -1,72 +1,77 @@
 
 package programabanco;
 public class Ahorro {
-    double monto;
-    double interes;
+    private double monto;
+    private double interes;
 
     public Ahorro(int monto){
             this.monto=monto;
     }
     public double cuenta_de_ahorro(){
         if(monto<10000){
-            interes=0.01;
+            interes=1;
         }else
         {
             if(monto>=10000 && monto<15000){
-                interes=0.02;
+                interes=2;
             } else {
                     if(monto>=15000 && monto<20000){
-                        interes=0.03;
+                        interes=3;
                     }else
                         {if(monto>=20000 && monto<25000){
-                            interes=0.05;
+                            interes=5;
                         }else
-                        {interes=0.07;}}}}
-        System.out.println("Porcentaje de interes: " + interes +"%");
+                        {interes=7;}}}}
         return interes;
     }
     public double cuenta_sueldo(){
         if(monto<10000){
-            interes=0.03;
+            interes=3;
         }else
         {
             if(monto>=10000 && monto<15000){
-                interes=0.05;
+                interes=5;
             } else {
                     if(monto>=15000 && monto<20000){
-                        interes=0.07;
+                        interes=7;
                     }else
                         {if(monto>=20000 && monto<25000){
-                            interes=0.09;
+                            interes=9;
                         }else
-                        {interes=0.11;}}}}
+                        {interes=11;}}}}
         return interes;
     }
     public double cuenta_premium(){
         if(monto<10000){
-            interes=0.035;
+            interes=3.5;
         }else
         {
             if(monto>=10000 && monto<15000){
-                interes=0.06;
+                interes=6;
             } else {
                     if(monto>=15000 && monto<20000){
-                        interes=0.075;
+                        interes=7.5;
                     }else
                         {if(monto>=20000 && monto<25000){
-                            interes=0.1;
+                            interes=10;
                         }else
-                        {interes=0.125;}}}}
+                        {interes=12.5;}}}}
         return interes;
     }
     public double setdeposito(float monto_op){
         monto=monto+monto_op;
+        System.out.println("Deposito: "+monto_op);
         System.out.println("Monto final: "+monto);
         return monto;
     }
     public double setretiro(int monto_op){
+        if (monto_op<=monto){
         monto=monto-monto_op;
+        System.out.println("Retiro: "+monto_op);
         System.out.println("Monto final: "+monto);
+        } else {
+            System.out.println("La cantidad que desea retirar excede al monto actual de su cuenta");
+        } 
         return monto;
     }
     public double getmonto(){
