87fb53aa34a3301c84455566223231b5d9ab64dd
gabyvipa05 <danna.villavicencio@alum.udep.edu.pe>
2023-08-27 20:31:41 -0500

    ahorro

--- src/programabanco/Cuenta_de_ahorro.java
+++ src/programabanco/Cuenta_de_ahorro.java
@@ -3,12 +3,35 @@ package programabanco;
 public class Cuenta_de_ahorro {
     String numero_cuenta;
     String nombre;
+    Ahorro ahorro;
+    
     
     public Cuenta_de_ahorro(){
-        nombre="Cuenta de ahorro";
+        nombre="Cuenta Ahorro";
     }
     
-    public Cuenta_de_ahorro(String numero_cuenta){
+    public Cuenta_de_ahorro(String numero_cuenta, String nombre, int monto){
         this.numero_cuenta=numero_cuenta;
-    }    
+        this.nombre=nombre;
+        this.ahorro=new Ahorro(monto);
+    }
+    
+    public double setdeposito(int monto_op){
+        return ahorro.setdeposito(monto_op);
+    }
+    
+    public double setretiro(int monto_op){
+        return ahorro.setretiro(monto_op);
+    }
+    
+    public void imprimir_interes(){
+    System.out.println("Nuevo interes: "+ ahorro.cuenta_de_ahorro());
+    }
+    
+    public void imprimir_datos(){
+        System.out.println("Numero de cuenta: " + numero_cuenta);
+        System.out.println("Nombre de la cuenta: " + nombre);
+        System.out.println("Monto de la cuenta: " + ahorro.getmonto());
+        System.out.println("Interes de la cuenta: " + ahorro.cuenta_de_ahorro());
+    }
 }
