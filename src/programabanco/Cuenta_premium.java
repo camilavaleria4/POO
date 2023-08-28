28536546fe15a560f32537566f54202510889677
camilavaleria4 <camilavaleria4@hotmail.com>
2023-08-27 21:23:04 -0500

    Avance cuenta premium

--- src/programabanco/Cuenta_premium.java
+++ src/programabanco/Cuenta_premium.java
@@ -1,12 +1,14 @@
 package programabanco;
 
 public class Cuenta_premium {
-    String numero_cuenta;
-    String nombre;
-    Ahorro ahorro;
+    private String numero_cuenta;
+    private String nombre;
+    private Ahorro ahorro;
     
     public Cuenta_premium(){
+        numero_cuenta="00000";
         nombre="Cuenta premium";
+        ahorro=new Ahorro(0);
     }
     
     public Cuenta_premium(String numero_cuenta, String nombre, int monto){
@@ -23,13 +25,13 @@ public class Cuenta_premium {
     }
     
     public void imprimir_interes(){
-        System.out.println("Nuevo interes: "+ ahorro.cuenta_premium());
+        System.out.println("Nuevo interes: "+ ahorro.cuenta_premium()+"%");
     }
     
     public void imprimir_datos(){
         System.out.println("Numero de cuenta: " + numero_cuenta);
         System.out.println("Nombre de la cuenta: " + nombre);
         System.out.println("Monto de la cuenta: " + ahorro.getmonto());
-        System.out.println("Interes de la cuenta: " + ahorro.cuenta_premium());
+        System.out.println("Interes de la cuenta: " + ahorro.cuenta_premium()+"%");
     }
 }
